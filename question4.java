a2e0a95c9c90b20ee5da4ec49bf804e81713ed33
SAM <90522738+venomusblood568@users.noreply.github.com>
2023-05-07 01:31:39 +0530

    java-written-in-java-main

---
+++ question4.java
@@ -0,0 +1,10 @@
+package com.coding;
+
+public class question4 {
+    public static void main(String[] args) {
+        System.out.println("    J      A        V      V     A");
+        System.out.println("    J     A A        V    V     A A");
+        System.out.println("J    J   A A A A      V  V    A A A A");
+        System.out.println(" J J    A       A       V    A       A");
+    }
+}
